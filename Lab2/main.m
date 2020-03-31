b9cd26d20d41566b5518ccbb54377f095c56f032
WendaLi <lwd19861127@hotmail.com>
2020-03-31 10:12:57 +0800

    Initial Commit

---
+++ Lab2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab2
+//
+//  Created by WendaLi on 2020-03-31.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
